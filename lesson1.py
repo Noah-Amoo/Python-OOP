69b39667580000a2318c174196d550fb24ac5057
Noah Amoo <yawnoah3n@yahoo.com>
2025-02-28 04:13:36 +0000

    Added age function

--- lesson1.py
+++ lesson1.py
@@ -1,15 +1,19 @@
 class Dog:
 
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
 
     def get_name(self):
         return self.name
+    
+    def get_age(self):
+        return self.age
 
     
 
-d1 = Dog('Frank')
-d2 = Dog('Mercy')
+d1 = Dog('Frank', 3)
+d2 = Dog('Mercy', 7)
 
-print(d1.get_name())
-print(d2.get_name())
+print(d1.get_name(), d1.get_age())
+print(d2.get_name(), d2.get_age())
